facf0db65ae19558c54e7b1f780c0b3fd1b7c91d
redshot <adrianberenguel90@gmail.com>
2021-06-13 17:00:44 +0800

    Added babel loader in the comments section

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import getGreeting from './greet'
+import getGreeting from "./greet";
 
 // Files in bin directory are executable
 // By default webpack is gonna create a dist directory, where output will go
 // We need a compiler/transpiler as part of the process to help transform modern javascript code that will run on older browsers
-console.log(getGreeting('world'));
+console.log(getGreeting("world"));
